f4a8f6d5712b9e3bb341c1cbf7996de02dc4762c
zrubenst <zrubenst@gmail.com>
2018-12-02 18:50:54 -0500

    Adds documentation comments to /slate and /exchange

--- exchange/index.js
+++ exchange/index.js
@@ -1,3 +1,6 @@
 
+/*
+ * Exports Exchange at `dolomite/exchange` along with other classes ( { Balance } for example)
+ */
 var exchange = require('../lib/Exchange/Exchange');
 require('../lib/common/util/exportPackage')(exports, exchange);
--- slate/index.js
+++ slate/index.js
@@ -1,3 +1,6 @@
 
+/*
+ * Exports Slate at `dolomite/slate` along with other classes ( { Period } for example)
+ */
 var slate = require('../lib/Slate/Slate');
 require('../lib/common/util/exportPackage')(exports, slate);
